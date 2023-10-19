9ca96af64b1fb34b62e6f3cc842364be9055bacd
Ran3022 <139870642+Ran3022@users.noreply.github.com>
2023-10-19 13:53:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const byteSize = (str) => {
   // write your code here
+	const blob= new blob([str]);
+	const sizeInByte = blob.size; 
+
+	return sizeInByte;
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
+
